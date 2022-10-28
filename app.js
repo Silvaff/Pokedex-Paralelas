343744df573d5331572f95cb5f03ac7ee7b5cbca
Matypinkman <55607185+Matypinkman@users.noreply.github.com>
2022-10-28 17:21:04 -0300

    Version paginado y busqueda, inicio bootstrap

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
-let pokelista = document.getElementById("ListaPoke")
+const pokelista = document.getElementById("ListaPoke")
 const botones=document.getElementById("botones");
 const botonlista=document.getElementById("botonlista");
+const BusquedaPoke=document.getElementById("BusquedaPoke");
 
-function imprimirarray(arreglo){
- for(let ai of arreglo){
-  pokelista.innerHTML+=`
-               <p> ${ai.type.name}</p> 
-               `
- }
-}
+
 
 
 function LlamadaApi(url) {
@@ -22,37 +17,39 @@ function LlamadaApi(url) {
           .then(x=>{
             let tipo="";
             x.types.forEach(element => {
-              tipo+=`${element.type.name}<br>`;
+              tipo+=`<br>${element.type.name}`;
             });
             let formas="";
             x.forms.forEach(element =>{
-              formas+=`${element.name}<br>`;
+              formas+=`<br>${element.name}`;
             });
             let habilidades="";
             x.abilities.forEach(element =>{
-              habilidades+=`${element.ability.name} <br>`;
+              habilidades+=`<br>${element.ability.name} `;
             });
 
 
             pokelista.innerHTML+= `<div class="card">
-            <img src="${x.sprites.front_default}">
-            <p>ID ${x.id}</p>
-            <p>Nombre ${x.name}</p>
-            <p>Altura ${x.height/10} m</p>
-            <p>Peso ${x.weight/10} Kg</p>
-            <p>Tipo: ${tipo} </p>        
-            <p>Formas: ${formas}</p>  
-            <p>Habilidades: ${habilidades}</p>  
-            <p>Ubicacion: </p>
+          <img src="${x.sprites.front_default}" class="card-img-top">
+           <h5 class="card-title text-center">ID ${x.id} <br> ${x.name}</h5>
+           <p class="card-text text-center">         
+           Altura ${x.height/10} 
+           Peso ${x.weight/10} Kg <br>
+           Tipo: ${tipo}       
+           <br>Formas: ${formas}
+           <br> Habilidades: ${habilidades}
+           <br>   Ubicacion: </p>
             </div>`
           
           });
       };
         // Mostramos Los botones a los enlaces de siguiente o anterior de la paginacion de los pokemones 
         //Boton hacia atrás
-        botones.innerHTML = (res.previous) ? `<button onclick="LlamadaApi('${res.previous}')">Atrás</button>` : "";
+        botones.innerHTML = (res.previous) ? `<button type="button" onclick="LlamadaApi('${res.previous}')" class="btn btn-secondary">Atrás</button>` : "";
+        
         //Botón hacia adelante
-        botones.innerHTML += (res.next) ? `<button onclick="LlamadaApi('${res.next}')">Siguiente</button>` : "";
+        botones.innerHTML += (res.next) ? `<button type="button" onclick="LlamadaApi('${res.next}')" class="btn btn-secondary  position-relative">Siguiente</button>` : "";
+        
   });
 }
 
@@ -68,32 +65,33 @@ function buscar(){
  .then(res=> {
   let tipo="";
   res.types.forEach(element => {
-    tipo+=`${element.type.name}<br>`;
+    tipo+=`<br> ${element.type.name}`;
   });
   let formas="";
   res.forms.forEach(element =>{
-    formas+=`${element.name}<br>`;
+    formas+=`<br> ${element.name}`;
   });
   let habilidades="";
   res.abilities.forEach(element =>{
-    habilidades+=`${element.ability.name} <br>`;
+    habilidades+=` <br>${element.ability.name}`;
   });
 
 
   BusquedaPoke.innerHTML+= `<div class="card">
-  <img src="${res.sprites.front_default}">
-  <p>ID ${res.id}</p>
-  <p>Nombre ${res.name}</p>
-  <p>Altura ${res.height/10} m</p>
-  <p>Peso ${res.weight/10} Kg</p>
-  <p>Tipo: ${tipo} </p>        
-  <p>Formas: ${formas}</p>  
-  <p>Habilidades: ${habilidades}</p>  
-  <p>Ubicacion: </p>
-  </div>`
-
-   botonlista.innerHTML+=`<a href="index.html">
-   <input type="submit" value="Inicio"/>
- </a>`
+<div class="row g-0">
+<div class="col-md-4">
+<img src="${res.sprites.front_default}" class="img-fluid rounded-start" width="300" height="400">
+</div>
+<div class="col-md-8">
+  <div class="card-body">
+    <h5 class="card-title">ID ${res.id} <br> ${res.name} </h5>
+    <p class="card-text"><p>Altura ${res.height/10} m</p>
+    <p>Peso ${res.weight/10} Kg</p>
+    <p>Tipo: ${tipo} </p>        
+    <p>Formas: ${formas}</p>  
+    <p>Habilidades: ${habilidades}</p>  
+  </div>
+</div> `
+   botonlista.innerHTML+=`<button  onclick="location.href='index.html';" type="button" class="btn btn-secondary btn-lg btn-block">Inicio</button>`
  })
 }
