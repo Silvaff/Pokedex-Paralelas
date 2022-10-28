8dcd815168e0a396e34971452a103882c83f68fe
Matypinkman <55607185+Matypinkman@users.noreply.github.com>
2022-10-28 17:10:38 -0300

    Version paginado y busqueda, inicio bootstrap

--- app.js
+++ app.js
@@ -1,51 +1,99 @@
+let pokelista = document.getElementById("ListaPoke")
 const botones=document.getElementById("botones");
-let pokelista=document.getElementById("ListaPoke");
+const botonlista=document.getElementById("botonlista");
 
-
-function listapoke(url){
-  pokelista.innerHTML="";
-  fetch(url)
-  .then(res=>res.json())
-  .then(res=> {
-    for(let i of res.results){
-      fetch(i.url)
-      .then(x=>x.json())
-      .then(x=>{
-        pokelista.innerHTML+= `
-        <img src="${x.sprites.front_default}">
-        <p>ID ${x.id}</p>
-        <p>Nombre ${x.name}</p>
-        <p>Altura ${x.height/10} m</p>
-        <p>Peso ${x.weight/10} Kg</p>
-        <p>Tipo: `
-        for( let t of x.types){
-          pokelista.innerHTML+=`
-           <p> ${t.type.name}</p> 
-           `
-         }
-        pokelista.innerHTML+=`</p>
-        <p>Formas: `
-        for( let f of x.forms){
-          pokelista.innerHTML+=`
-           <p> ${f.name}</p> 
-           `
-         }
-        pokelista.innerHTML+=`</p>
-        <p>Habilidades:`
-        for( let a of x.abilities){
-          pokelista.innerHTML+=`
-           <p> ${a.ability.name}</p> 
-           `
-         }
-        pokelista.innerHTML+=`</p>
-        <p>Ubicacion </p>
-      </div>`
-      })
-      botones.innerHTML = (res.previous) ? `<button onclick="listapoke('${res.previous}')">Atrás</button>` : "";
-      botones.innerHTML += (res.next) ? `<button onclick="listapoke('${res.next}')">Siguiente</button>` : "";
-    }
-  })
+function imprimirarray(arreglo){
+ for(let ai of arreglo){
+  pokelista.innerHTML+=`
+               <p> ${ai.type.name}</p> 
+               `
+ }
 }
 
-listapoke('https://pokeapi.co/api/v2/pokemon?offset=0&limit=10');
 
+function LlamadaApi(url) {
+  pokelista.innerHTML="";
+    fetch(url)
+    .then(res=>res.json())
+    .then(res=> {
+      for(let i of res.results){
+        fetch(i.url)
+          .then(x=>x.json())
+          .then(x=>{
+            let tipo="";
+            x.types.forEach(element => {
+              tipo+=`${element.type.name}<br>`;
+            });
+            let formas="";
+            x.forms.forEach(element =>{
+              formas+=`${element.name}<br>`;
+            });
+            let habilidades="";
+            x.abilities.forEach(element =>{
+              habilidades+=`${element.ability.name} <br>`;
+            });
+
+
+            pokelista.innerHTML+= `<div class="card">
+            <img src="${x.sprites.front_default}">
+            <p>ID ${x.id}</p>
+            <p>Nombre ${x.name}</p>
+            <p>Altura ${x.height/10} m</p>
+            <p>Peso ${x.weight/10} Kg</p>
+            <p>Tipo: ${tipo} </p>        
+            <p>Formas: ${formas}</p>  
+            <p>Habilidades: ${habilidades}</p>  
+            <p>Ubicacion: </p>
+            </div>`
+          
+          });
+      };
+        // Mostramos Los botones a los enlaces de siguiente o anterior de la paginacion de los pokemones 
+        //Boton hacia atrás
+        botones.innerHTML = (res.previous) ? `<button onclick="LlamadaApi('${res.previous}')">Atrás</button>` : "";
+        //Botón hacia adelante
+        botones.innerHTML += (res.next) ? `<button onclick="LlamadaApi('${res.next}')">Siguiente</button>` : "";
+  });
+}
+
+
+LlamadaApi("https://pokeapi.co/api/v2/pokemon?offset=0&limit=10");
+
+function buscar(){
+  BusquedaPoke.innerHTML="";
+  botonlista.innerHTML="";
+ var idpoke= document.getElementById("pokeid").value;
+ fetch(`https://pokeapi.co/api/v2/pokemon/${idpoke}/`)
+ .then(res=>res.json())
+ .then(res=> {
+  let tipo="";
+  res.types.forEach(element => {
+    tipo+=`${element.type.name}<br>`;
+  });
+  let formas="";
+  res.forms.forEach(element =>{
+    formas+=`${element.name}<br>`;
+  });
+  let habilidades="";
+  res.abilities.forEach(element =>{
+    habilidades+=`${element.ability.name} <br>`;
+  });
+
+
+  BusquedaPoke.innerHTML+= `<div class="card">
+  <img src="${res.sprites.front_default}">
+  <p>ID ${res.id}</p>
+  <p>Nombre ${res.name}</p>
+  <p>Altura ${res.height/10} m</p>
+  <p>Peso ${res.weight/10} Kg</p>
+  <p>Tipo: ${tipo} </p>        
+  <p>Formas: ${formas}</p>  
+  <p>Habilidades: ${habilidades}</p>  
+  <p>Ubicacion: </p>
+  </div>`
+
+   botonlista.innerHTML+=`<a href="index.html">
+   <input type="submit" value="Inicio"/>
+ </a>`
+ })
+}
